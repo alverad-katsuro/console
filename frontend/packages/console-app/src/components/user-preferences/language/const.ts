e1b09aced38e1841b64c885caa674cb870cb98fd
Alfredo Gabriel <amgabriel1@hotmail.com>
2024-07-14 15:33:09 -0300

    feat: add portugues select

--- frontend/packages/console-app/src/components/user-preferences/language/const.ts
+++ frontend/packages/console-app/src/components/user-preferences/language/const.ts
@@ -2,6 +2,7 @@ export const supportedLocales = {
   en: 'English',
   es: 'Español - Spanish',
   fr: 'Français - French',
+  'pt-BR': 'Português - Brasil',
   ko: '한국어 - Korean',
   ja: '日本語 - Japanese',
   'zh-CN': '中文 - Chinese (Simplified)',
